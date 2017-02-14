9e40dcfa884ea93864d47aa9708c7feb583981ad
Emanoel <emanoel.lopes@me.com.br>
2017-02-14 10:43:57 -0200

    Continue block binds chapter

---
+++ 01-block-bindings/block-binding-in-loops.js
@@ -0,0 +1,10 @@
+/* Block Binding in Loops */
+'use strict';
+
+const myArr = ['cat', 'dog', 'elephant', 'cammel'];
+
+for (let i = 0; i < myArr.length; i++) {
+  console.log(myArr[i]);
+};
+
+//console.log(i); // I value it's not accessible here, it will throw an error
